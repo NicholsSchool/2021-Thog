dc0591ddfb2f63d63a31993750b110fe3b916c61
lbrannen <lexicb@icloud.com>
2021-02-27 12:51:33 -0500

    revolver code

--- src/main/java/frc/robot/subsystems/BallLift.java
+++ src/main/java/frc/robot/subsystems/BallLift.java
@@ -1,9 +1,25 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.SparkMax;
+
 public class BallLift {
+    private SparkMax ballLift;
+    private double speed;
 
     public BallLift(){
+        ballLift = new SparkMax();      
+    }
 
+    public void lift(){
+        move();
+    }
+
+    private void move(){
+        ballLift.set(speed);
+    }
+
+    public void stop(){
+        ballLift.stopMotor();
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/Revolver.java
+++ src/main/java/frc/robot/subsystems/Revolver.java
@@ -4,11 +4,23 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Revolver extends SubsystemBase{
+
     private WPI_TalonSRX revolver;
-
-
+    private double speed;
     public Revolver(){
         revolver = new WPI_TalonSRX (22);
+    }
+
+    public void spin(){
+        move();
+    }
+
+    private void move(){
+        revolver.set(speed);
+    }
+
+    public void stop(){
+        revolver.stopMotor();
     }
 
 
