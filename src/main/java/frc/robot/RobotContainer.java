48ae1d624195e7290ee71af72262da437750d6e5
Camden Bartlo <camdenbartlo@Camdens-MacBook-Air.local>
2021-02-27 18:11:06 -0500

    Fixed Ball-Lift

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import frc.robot.subsystems.BallLift;
+
+public class RobotContainer {
+
+    public static BallLift ballLift;
+
+    public RobotContainer() {
+
+        ballLift = new BallLift();
+    }
+}
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,8 +1,8 @@
 package frc.robot;
 
 public class RobotMap {
-    public static final int REVOLVER_ID = 22;
 
     public static final int BALL_LIFT_ID = 44;
 
+    public static final double BALL_LIFT_SPEED = 0.5;
 }
