c27b4f24d9ff88ce719c1d0732031b48c9ef8646
lbrannen <lexicb@icloud.com>
2021-02-27 13:54:29 -0500

    robot map IDs

--- src/main/java/frc/robot/subsystems/BallLift.java
+++ src/main/java/frc/robot/subsystems/BallLift.java
@@ -2,13 +2,14 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
 
 public class BallLift extends SubsystemBase{
     private CANSparkMax ballLift;
     private double speed;
 
     public BallLift(){
-        ballLift = new CANSparkMax(44, null);
+        ballLift = new CANSparkMax(RobotMap.BALL_LIFT_ID, null);
     }
 
     public void lift(){
--- src/main/java/frc/robot/subsystems/Revolver.java
+++ src/main/java/frc/robot/subsystems/Revolver.java
@@ -2,13 +2,14 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
 
 public class Revolver extends SubsystemBase{
 
     private WPI_TalonSRX revolver;
     private double speed;
     public Revolver(){
-        revolver = new WPI_TalonSRX (22);
+        revolver = new WPI_TalonSRX (RobotMap.REVOLVER_ID);
     }
 
     public void spin(){
