1d09245bf14910cf219d175408355cc031e1f864
lbrannen <lexicb@icloud.com>
2021-02-27 14:53:29 -0500

    shooter code

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,5 +4,13 @@ public class RobotMap {
     public static final int REVOLVER_ID = 22;
 
     public static final int BALL_LIFT_ID = 44;
+    public static final int SHOOTER_ID = 0;
 
+    public static final int SHOOTER_F = 0;
+    public static final int SHOOTER_P = 0;
+    public static final int SHOOTER_I = 0;
+    public static final int SHOOTER_D = 0;
+
+    public static final int SHOOTER_RAMP_TIME =0;
+    public static final int SHOOT_VELOCITY = 0;
 }
