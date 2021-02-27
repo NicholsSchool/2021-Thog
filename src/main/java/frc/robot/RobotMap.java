7fa1f59ddd4f71387ed6a82f3b6da972b73eb283
Camden Bartlo <camdenbartlo@Camdens-MacBook-Air.local>
2021-02-27 16:22:44 -0500

    Intake & Whatnot

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,4 +7,16 @@ public class RobotMap {
 
     public static final int RIGHT_MASTER_ID = 22;
     public static final int RIGHT_SLAVE_ID = 23;
+
+    public static final int LEFT_INTAKE_ID = 40;
+    public static final int RIGHT_INTAKE_ID = 41;
+
+    public static final int PCM_CAN_ID = 51;
+
+    public static final int INTAKE_SOLENOID_CHANNEL = 1;
+
+    public static final boolean INTAKE_DOWN = true;
+    public static final boolean INTAKE_UP = !INTAKE_DOWN;
+
+    public static final double INTAKE_SPEED = 0.5;
 }
---
+++ src/main/java/frc/robot/commands/TakeIn.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.RobotMap;
+
+public class TakeIn extends CommandBase {
+    
+    public TakeIn() {
+
+        addRequirements(RobotContainer.intake);
+    }
+
+    @Override
+    public void initialize() {}
+    
+    @Override
+    public void execute() {
+        RobotContainer.intake.move(RobotMap.INTAKE_SPEED);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+        RobotContainer.intake.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        
+        return false;
+    }
+}
