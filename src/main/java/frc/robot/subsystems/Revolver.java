5ddbbcd86a4836b4193216a08bb78cc674f5a888
Camden Bartlo <camdenbartlo@Camdens-MacBook-Air.local>
2021-02-27 21:30:30 -0500

    FX -> SRX in Revolver.java

--- src/main/java/frc/robot/subsystems/Revolver.java
+++ src/main/java/frc/robot/subsystems/Revolver.java
@@ -1,17 +1,17 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class Revolver extends SubsystemBase {
     
-    private WPI_TalonFX revolver;
+    private WPI_TalonSRX revolver;
 
     public Revolver() {
 
-        revolver = new WPI_TalonFX(RobotMap.REVOLVER_ID);
+        revolver = new WPI_TalonSRX(RobotMap.REVOLVER_ID);
         
         revolver.configFactoryDefault();
     }
