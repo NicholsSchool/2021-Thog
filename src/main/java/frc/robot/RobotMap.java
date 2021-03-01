b904d9f496d0042dfb3361a4e33954646bff0f76
Kiddy, Jonathan <jon.kiddy@roswellpark.org>
2021-03-01 16:56:15 -0500

    Updated CAN IDs

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,16 +3,16 @@ package frc.robot;
 public class RobotMap {
 
     // Ball Lift
-    public static final int BALL_LIFT_ID = 44;
+    public static final int BALL_LIFT_ID = 43; //set
 
     public static final double BALL_LIFT_SPEED = 0.5;
 
     // DriveTrain
-    public static final int LEFT_MASTER_ID = 20;
-    public static final int LEFT_SLAVE_ID = 21;
+    public static final int LEFT_MASTER_ID = 33; //set
+    public static final int LEFT_SLAVE_ID = 32; //set
 
-    public static final int RIGHT_MASTER_ID = 22;
-    public static final int RIGHT_SLAVE_ID = 23;
+    public static final int RIGHT_MASTER_ID = 34; //set
+    public static final int RIGHT_SLAVE_ID = 30; //set
 
     public static final int SHIFTER_SOLENOID_CHANNEL = 0;
 
@@ -20,16 +20,17 @@ public class RobotMap {
     public static final boolean LOW_GEAR = !HIGH_GEAR;
 
     // General
-    public static final int PCM_CAN_ID = 51;
+    public static final int PCM_CAN_ID = 51; //set
+    public static final int PDP_CAN_ID = 50; //set
 
     // Hood
-    public static final int HOOD_ID = 45;
+    public static final int HOOD_ID = 42; //set
     
-    public static final double HOOD_SPEED = 0.1;
+    public static final double HOOD_SPEED = 0.5;
 
     // Intake
-    public static final int LEFT_INTAKE_ID = 40;
-    public static final int RIGHT_INTAKE_ID = 41;
+    public static final int LEFT_INTAKE_ID = 40; //set
+    public static final int RIGHT_INTAKE_ID = 41; //set
 
     public static final int INTAKE_SOLENOID_CHANNEL = 1;
 
@@ -39,12 +40,18 @@ public class RobotMap {
     public static final double INTAKE_SPEED = 0.5;
         
     // Revolver
-    public static final int REVOLVER_ID = 24;
+    public static final int REVOLVER_ID = 20; //set
 
-    public static final double REVOLVER_SPEED = 0.25;
+    public static final double REVOLVER_SPEED = 0.5;
 
     // Shooter
-    public static final int SHOOTER_ID = 25;
+    public static final int SHOOTER_ID = 31; //set
 
     public static final double SHOOT_SPEED = 0.5;
+
+    // Elevator Left: 21
+    // Elevator Right: 22
+
+    // Spare Talon: 29
+    // Spare SparkMax: 49
 }
